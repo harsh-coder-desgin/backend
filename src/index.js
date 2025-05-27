53f6173c5c120c875a747ad4e6c469eb0be07635
harsh-coder-desgin <harshpatel51057@gmail.com>
2025-05-27 19:01:37 +0530

    backend work

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 // require('dotenv').config({path:'/env'})
+
+
+
+
+
+
+
+
+
+
+
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
@@ -6,6 +17,50 @@ dotenv.config({
     path:'./env'
 }) 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is runing at port : ${process.env.PORT}`);
+    })
+    app.on("error",(error)=>{
+            console.log("error",error);
+            throw error
+        })
+})
+.catch((err)=>{
+    console.log("mongo db connection failed",err)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // import express from "express"
